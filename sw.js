c27226c864f16a8f0c9a2f00f3c883a14c797660
Elias Binefeld <binefeld.elias.media@gmail.com>
2026-09-09 17:07:54 +0200

    Anmeldeschirm blendet sich nach der Anmeldung wirklich aus

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Service Worker: macht die App installierbar und startfaehig ohne Netz.
    Nur das Geruest wird zwischengespeichert - niemals Daten aus Google. */
-const CACHE = "schnittplan-v1";
+const CACHE = "schnittplan-v2";
 const GERUEST = [
   "./",
   "./index.html",
